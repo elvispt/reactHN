be5123cd0c0a81b1192b5c0f8f851e91b502d9f0
Elvis Pestana <elvispt@gmail.com>
2018-11-15 22:35:22 +0000

    Add component for story title and header
    Closes #3

---
+++ src/components/_story.js
@@ -0,0 +1,26 @@
+export class Story extends React.Component {
+  render() {
+    return (
+      <div className="story">
+        <div>
+          TITLE <small>(domain.com)</small>
+          <div>
+            <small>
+              <span>123</span> | <span>2 hours ago</span> | <span>123 comments</span>
+            </small>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam dolorem facilis ipsam maiores minima
+          molestiae, necessitatibus numquam praesentium quam quasi reiciendis similique tenetur velit voluptas? Alias
+          architecto asperiores aspernatur assumenda beatae consequatur eligendi et, fugiat ipsa iure laboriosam modi
+          mollitia nesciunt possimus praesentium recusandae rem similique sunt tempore ut vel vitae, voluptates. Amet
+          consectetur eos, fugit ipsam iure minima minus omnis perspiciatis placeat porro quidem, quo quod tempore ullam
+          ut vitae voluptate voluptatum? Dolore ipsam, iusto labore nobis possimus quaerat quas quos! Adipisci
+          asperiores consequatur dolor doloremque excepturi facere iste minima, odit porro provident, quam quos rerum
+          velit, veritatis?
+        </p>
+      </div>
+    );
+  }
+}
